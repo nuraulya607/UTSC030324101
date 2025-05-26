7b2511c536b7c01ada04909e6198319a6c05630f
nurauliawati <nuraulya607@gmail.com>
2025-05-27 00:17:46 +0800

    commit bertahap

--- menu.cpp
+++ menu.cpp
@@ -2,10 +2,6 @@
 #include <conio.h>
 using namespace std;
 
-const int MAX = 100;
-int data[MAX];
-int jumlahData = 0;
-
 void menu() {
     system("cls");
     cout << "== Aplikasi Sorting Bubble ==\n";
@@ -17,83 +13,11 @@ void menu() {
     cout << "Pilih [1-5]: ";
 }
 
-void inputData() {
-    system("cls");
-    cout << "Jumlah data: ";
-    cin >> jumlahData;
-    if (jumlahData <= 0 || jumlahData > MAX) {
-        cout << "Jumlah tidak valid!";
-        getch();
-        return;
-    }
-
-    for (int i = 0; i < jumlahData; i++) {
-        cout << "Data ke-" << i + 1 << ": ";
-        cin >> data[i];
-    }
-    cout << "Data berhasil dimasukkan.";
-    getch();
-}
-
-void tampilData() {
-    system("cls");
-    if (jumlahData == 0) {
-        cout << "Belum ada data.";
-    } else {
-        cout << "Data:\n";
-        for (int i = 0; i < jumlahData; i++)
-            cout << data[i] << " ";
-    }
-    getch();
-}
-
-void sortingData() {
-    system("cls");
-    if (jumlahData == 0) {
-        cout << "Data kosong.";
-        getch();
-        return;
-    }
-
-    // Bubble sort ascending
-    for (int i = 0; i < jumlahData - 1; i++) {
-        for (int j = 0; j < jumlahData - i - 1; j++) {
-            if (data[j] > data[j + 1]) {
-                int tmp = data[j];
-                data[j] = data[j + 1];
-                data[j + 1] = tmp;
-            }
-        }
-    }
-
-    cout << "Data setelah sorting:\n";
-    for (int i = 0; i < jumlahData; i++)
-        cout << data[i] << " ";
-    getch();
-}
-
-void sepatahKata() {
-    system("cls");
-    cout << "Indonesia Bisa, Kita Juara!\n@poliban";
-    getch();
-}
-
 int main() {
     char pilih;
     do {
         menu();
         pilih = getch();
-        switch (pilih) {
-            case '1': inputData(); break;
-            case '2': tampilData(); break;
-            case '3': sortingData(); break;
-            case '4': sepatahKata(); break;
-            case '5': break;
-            default:
-                system("cls");
-                cout << "Pilihan tidak tersedia!";
-                getch();
-        }
     } while (pilih != '5');
     return 0;
 }
