5c0878185ca48c7c5d4e611aa44fee034f3d69ac
nurauliawati <nuraulya607@gmail.com>
2025-05-27 00:39:06 +0800

    commit bertahap

--- menu.cpp
+++ menu.cpp
@@ -2,6 +2,10 @@
 #include <conio.h>
 using namespace std;
 
+const int MAX = 100;
+int data[MAX];
+int jumlahData = 0;
+
 void menu() {
     system("cls");
     cout << "== Aplikasi Sorting Bubble ==\n";
@@ -13,11 +17,44 @@ void menu() {
     cout << "Pilih [1-5]: ";
 }
 
+void inputData() {
+    system("cls");
+    cout << "Jumlah data: ";
+    cin >> jumlahData;
+    if (jumlahData <= 0 || jumlahData > MAX) {
+        cout << "Jumlah tidak valid!";
+        getch();
+        return;
+    }
+    for (int i = 0; i < jumlahData; i++) {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> data[i];
+    }
+    cout << "Data berhasil dimasukkan.";
+    getch();
+}
+
+void tampilData() {
+    system("cls");
+    if (jumlahData == 0) {
+        cout << "Belum ada data.";
+    } else {
+        cout << "Data:\n";
+        for (int i = 0; i < jumlahData; i++)
+            cout << data[i] << " ";
+    }
+    getch();
+}
+
 int main() {
     char pilih;
     do {
         menu();
         pilih = getch();
+        switch (pilih) {
+            case '1': inputData(); break;
+            case '2': tampilData(); break;
+        }
     } while (pilih != '5');
     return 0;
 }
